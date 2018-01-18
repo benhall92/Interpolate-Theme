5aeb0b567f034336ce6a47d6e41c63011ecbf9d3
benjamin hall <ben.hall@interpolate.co>
2018-01-18 16:26:39 +0000

    added font awesome as JS

--- inc/functions/enqueue-scripts.php
+++ inc/functions/enqueue-scripts.php
@@ -24,14 +24,15 @@ function inter_scripts() {
 
 	wp_enqueue_style( 'theme', $css_dir.'main.min.css', array(), false, false );
 
-	wp_enqueue_style( 'fontawesome-5', '//use.fontawesome.com/releases/v5.0.1/css/all.css', array(), false, false );
+	// wp_enqueue_style( 'fontawesome-5', '//use.fontawesome.com/releases/v5.0.1/css/all.css', array(), false, false );
 
 	wp_enqueue_script( 'jquery' );
+
+	wp_enqueue_script( 'font-awesome', '//use.fontawesome.com/releases/v5.0.4/js/all.js', array(), null );
 
 	wp_enqueue_script( 'theme', $js_dir.'theme.js', array('jquery'), false, true );
 
 	wp_enqueue_script( 'bootstrap-js', $js_dir.'bootstrap.min.js', array('jquery'), false, false );
-
 }
 
 add_action( 'wp_enqueue_scripts', 'inter_scripts', '11' );
@@ -44,5 +45,23 @@ function inter_admin_style(){
 }
 
 add_action('admin_head', 'inter_admin_style');
+
+/**
+ * Filter the HTML script tag of `font-awesome` script to add `defer` attribute.
+ *
+ * @param string $tag    The <script> tag for the enqueued script.
+ * @param string $handle The script's registered handle.
+ *
+ * @return   Filtered HTML script tag.
+ */
+// add_filter( 'script_loader_tag', 'add_defer_attribute', 10, 2 );
+
+function add_defer_attribute( $tag, $handle ) {
+
+    if ( 'font-awesome' === $handle ) {
+    	
+        return str_replace( ' src', ' defer src', $tag );
+    }
+}
 
 ?>
